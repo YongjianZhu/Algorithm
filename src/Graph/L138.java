017714fcbbc735dd73c79c74bdddecb0e7011cfc
Yongjian Zhu <yjzhu.sh@gmail.com>
2019-12-20 16:33:06 -0500

    138

---
+++ src/Graph/L138.java
@@ -0,0 +1,32 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class L138 {
+  public Node copyRandomList(Node head) {
+//    if (head == null) return null;
+//    Node node;
+//    node = head;
+//    HashMap<Node, Node> map = new HashMap<>();
+//
+//    ArrayList<Node> copyList = new ArrayList<>();
+//    while (node != null) {
+//      Node copy = new Node(node.val);
+//      copyList.add(copy);
+//      map.put(node, copy);
+//      node = node.next;
+//    }
+//
+//    node = head;
+//    while (node != null) {
+//      Node newNode = map.get(node);
+//      newNode.random = map.get(node.random);
+//      newNode.next = map.get(node.next);
+//      node = node.next;
+//    }
+//
+//    return map.get(head);
+    return null;
+  }
+}
