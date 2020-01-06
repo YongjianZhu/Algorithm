b89fc8373d09be7d4868fed406ad81f8c709ea8f
Yongjian Zhu <yjzhu.sh@gmail.com>
2020-01-06 15:24:27 -0500

    90

---
+++ src/Graph/L90.java
@@ -0,0 +1,27 @@
+package Graph;
+
+import java.util.*;
+
+public class L90 {
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> subset = new ArrayList<>();
+    List<Integer> sortedNums = new ArrayList<>();
+    for (int i : nums) sortedNums.add(i);
+    Collections.sort(sortedNums);
+    dfs(result, subset, sortedNums, 0);
+    return result;
+  }
+
+  public void dfs(List<List<Integer>> result,
+                  List<Integer> subset, List<Integer> nums, int position) {
+    if (!result.contains(subset)){
+      result.add(new ArrayList<>(subset));
+    }
+    for (int i = position; i < nums.size(); i++) {
+      subset.add(nums.get(i));
+      dfs(result, subset, nums, i + 1);
+      subset.remove(subset.size() - 1);
+    }
+  }
+}
