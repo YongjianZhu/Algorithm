0f745830758fd4553f0a68e5b7845cea82e4bcbf
Yongjian Zhu <yjzhu.sh@gmail.com>
2020-01-20 11:29:19 -0500

    141

---
+++ src/LinkedList/L141.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class L141 {
+  public boolean hasCycle(ListNode head) {
+    if (head == null) return false;
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (fast != null && fast.next != null) {
+      if (slow == fast) return true;
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return false;
+  }
+}
