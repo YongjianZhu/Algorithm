dd8c7c86ec1099d485ae38266a56f389b80c29d4
Yongjian Zhu <yjzhu.sh@gmail.com>
2020-03-05 00:58:49 -0500

    62

---
+++ src/DynamicProgramming/L62.java
@@ -0,0 +1,25 @@
+package DynamicProgramming;
+
+public class L62 {
+  public int uniquePaths(int m, int n) {
+    if (m < 1 || n < 1)
+      return 0;
+
+    int[][] matrix = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      matrix[0][i] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+      matrix[i][0] = 1;
+    }
+
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+      }
+    }
+
+    return matrix[m - 1][n - 1];
+  }
+}
